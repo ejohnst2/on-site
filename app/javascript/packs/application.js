fb3a579f80444f1925c04d35308dea993ba9dcd6
elijah johnston <eli@millinno.com>
2019-10-29 20:17:48 +0800

    first commit init, hitting end points from entry script

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,7 @@
 // const imagePath = (name) => images(name, true)
 
 console.log('Hello World from Webpacker')
+
+fetch('https://earthquake.usgs.gov/fdsnws/event/1/query.geojson?starttime=2019-10-22%2000:00:00&endtime=2019-10-29%2023:59:59&minmagnitude=2.5&orderby=time')
+  .then(response => response.json())
+  .then(data => console.log(data.features.slice(0,30)))
