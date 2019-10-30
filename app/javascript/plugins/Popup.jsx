fbccba0f06d79d5e6eefc63f0736a3cde51cac92
elijah johnston <eli@millinno.com>
2019-10-30 22:18:27 +0800

    popupwindow init

--- app/javascript/plugins/Popup.jsx
+++ app/javascript/plugins/Popup.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOMServer from "react-dom/server";
+import styled from 'styled-components';
+
+const Window = styled.div`
+  font-family: 'Open Sans', sans-serif;
+  max-width: 200px;
+  text-align: center;
+`
+
+const Title = styled.h3`
+`
+
+const Link = styled.a`
+`
 
 
 const Popup = (props) => {
   return (
-    <div>
-      <p>{props.title}</p>
-      <a href={props.details}>Check out more details</a>
-    </div>
+    <Window>
+      <Title>{props.title}</Title>
+      <Link href={props.details}>Check out more details</Link>
+    </Window>
   )
 }
 
