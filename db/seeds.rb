d9c96accc0fdaa60a4139783ac0eb97882f59f1b
elijah johnston <eli@millinno.com>
2019-10-30 17:00:29 +0800

    first 10 eqs

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ puts "getting url"
 uri = open('https://earthquake.usgs.gov/fdsnws/event/1/query.geojson?starttime=2019-10-22%2000:00:00&endtime=2019-10-29%2023:59:59&minmagnitude=2.5&orderby=time')
 
 puts "parsing results"
-results = JSON.parse(uri.read)
+results = JSON.parse(uri.read)["features"]
 
 puts "creating list"
 results.first(10).each do |e|
